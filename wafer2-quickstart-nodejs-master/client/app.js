790398d73ba43557d256deac6fec6a887eef5fff
35316225 <sjawjy@163.com>
2018-11-19 18:00:30 +0800

    gggg

--- wafer2-quickstart-nodejs-master/client/app.js
+++ wafer2-quickstart-nodejs-master/client/app.js
@@ -14,6 +14,13 @@ App({
         wx.getShareInfo({
             shareTicket: obj.shareTicket,
             success: function(res) {
+                // 对加密数据进行对称解密算法
+                /**
+                 *  对称解密使用的算法为 AES-128-CBC，数据采用PKCS#7填充。
+                    对称解密的目标密文为 Base64_Decode(encryptedData)。
+                    对称解密秘钥 aeskey = Base64_Decode(session_key), aeskey 是16字节。
+                    对称解密算法初始向量 为Base64_Decode(iv)，其中iv由数据接口返回。
+                 */
                 console.log(res)
             }
         })
